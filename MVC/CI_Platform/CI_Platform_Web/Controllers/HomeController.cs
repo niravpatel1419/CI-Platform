cf8da084556b3cf9d28c05ed19ddb7c6b7335005
Nirav H Patel <pct124@server7.com>
2023-02-13 18:11:37 +0530

    Created the ForgotPassword Page In MVC

--- MVC/CI_Platform/CI_Platform_Web/Controllers/HomeController.cs
+++ MVC/CI_Platform/CI_Platform_Web/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace CI_Platform_Web.Controllers
             return View();
         }
 
+        public IActionResult forgotPassword()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
