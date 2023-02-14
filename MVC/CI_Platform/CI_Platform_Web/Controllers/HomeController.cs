443c767bed5c84bf53e0609649a9572c654fa491
Nirav H Patel <pct124@server7.com>
2023-02-14 17:52:26 +0530

    Created the resetPassword Page in MVC

--- MVC/CI_Platform/CI_Platform_Web/Controllers/HomeController.cs
+++ MVC/CI_Platform/CI_Platform_Web/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace CI_Platform_Web.Controllers
             return View();
         }
 
+        public IActionResult resetPassword()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
